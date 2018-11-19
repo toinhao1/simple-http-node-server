9828174a6b5035a568b45eb436f3c0a205b7e0f0
toinhao1 <toinhao89@gmail.com>
2018-11-18 18:57:30 -0600

    Updates

---
+++ index.js
@@ -0,0 +1,7 @@
+const myLibrary = require('./my-library');
+myLibrary.myFirstMethod(;)
+
+const uuid = require('uuid');
+const newUuid = uuid();
+
+console.log('your new Universally unique identifier is ${newUuid}');
